87899ccd4dd60c87701c937169c0bd5c7d96267a
Tune H Pers <tunepers@broadinstitute.org>
2014-11-30 21:42:36 -0500

    Removed comment

--- depict.py
+++ depict.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
-import os,glob
+import os
 
 depict_jar = "/Users/tp/Google\ Drive/Projects/DEPICT/Code/DEPICT/dist/Depict.jar"
-path_to_depict_folder = "/Users/tp/Google\ Drive/Projects/DEPICT/Code/DEPICT_example"
+path_to_depict_folder = "/Users/tp/Google\ Drive/Projects/DEPICT/Code/DEPICT-example"
 study_label = "ldl_teslovich_nature2010"
 locus_file = "%s/%s.txt"%(path_to_depict_folder,study_label)
 path_results = path_to_depict_folder 
 ncores = 2
-gene_annotation = "ensembl_v75_GRCh37_depictaffymetrix.tab"
-depict_genelist_file = "ensembl_GRCh37_depictaffymetrix_genes.tab"
+gene_annotation = "GPL570ProbeENSGInfo+HGNC_reformatted.txt"
+depict_genelist_file = "GPL570ProbeENSGInfo+HGNC_reformatted.ens"
 reconstituted_genesets_file = "%s/data/reconstituted_genesets_example.txt"%path_to_depict_folder
 
 # Gene prioritization and reconstituted gene set enrichment analysis
