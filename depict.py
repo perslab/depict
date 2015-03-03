783ea24bd14a2dc328556e3545e95dae55855bb8
Tune Pers <broadinstitute.org>
2015-03-03 14:47:51 -0500

    Added 1KG support

--- depict.py
+++ depict.py
@@ -2,27 +2,27 @@
 #use Python-2.7
 
 import os
-from depict_library import get_loci
+from depict_library import write_loci
 
 depict_jar = "/cvar/jhlab/tp/DEPICT/dist/Depict.jar" # Path to DEPICT jar file (clone it from GitHub)
 depict_folder = os.getcwd() # Path to DEPICT-example (clone it from GibHub)
-collection_file = "/cvar/jhlab/snpsnap/data/step3/1KG_snpsnap_production_v1_uncompressed_incl_rsID/ld0.5/ld0.5_collection.tab" # Make compressed version
-depict_gene_file = "/cvar/jhlab/tp/depict/data/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.binary.rows.txt" # Copy file into Depict/data
-depict_gene_information_file = '/cvar/jhlab/tp/depict/data/ENSGToGeneNameHGNCBiotypeChromosomeStartStopStrandAndDescriptionV65.txt'
 
-study_label = "ldl_teslovich_nature2010"
-marker_file = "ldl_teslovich_nature2010.chrpos"
+study_label = "any_mig.gwama.out.isq75.nstud12.clean_p5e-8_loci_real_juha"
+marker_file = "%s.chrpos"%study_label 
 locus_file = "%s/%s_loci.txt"%(depict_folder,study_label)
 results_folder = depict_folder 
 ncores = 2
 gene_annotation = "GPL570ProbeENSGInfo+HGNC_reformatted.txt"
 depict_genelist_file = "GPL570ProbeENSGInfo+HGNC_reformatted.ens"
 reconstituted_genesets_file = "%s/data/reconstituted_genesets_example.txt"%depict_folder
+depict_gene_file = "%s/data/GPL570-GPL96-GPL1261-GPL1355TermGeneZScores-MGI_MF_CC_RT_IW_BP_KEGG_z_z.binary.rows.txt"%depict_folder
+depict_gene_information_file = "%s/data/ENSGToGeneNameHGNCBiotypeChromosomeStartStopStrandAndDescriptionV65.txt"%depict_folder
+collection_file = "%s/data/ld0.5_collection_depict_150302.txt"%depict_folder
 hla_start = 25000000
 hla_stop = 35000000
 
 # Construct DEPICT locus file
-get_loci(collection_file,depict_gene_file,depict_gene_information_file,marker_file,locus_file,hla_start,hla_stop) if not os.path.isfile(locus_file) else None
+write_loci(collection_file,depict_gene_file,depict_gene_information_file,marker_file,locus_file,hla_start,hla_stop) if not os.path.isfile(locus_file) else None
 
 # Gene prioritization and reconstituted gene set enrichment analysis
 #os.system("java -Xms512m -Xmx16000m -XX:+UseParallelGC -XX:ParallelGCThreads=3 -jar %s %s %s %s 1 1 0 %s %s %s %s %s "%(depict_jar,"%s/data/"%depict_folder,locus_file,study_label,ncores,results_folder,reconstituted_genesets_file,gene_annotation,depict_genelist_file))
--- depict_library.py
+++ depict_library.py
@@ -9,11 +9,6 @@ from bx.intervals.intersection import Interval
 from bx.intervals.intersection import IntervalTree
 
 
-# Read a collection
-def read_data(collectionfile):
-        return pd.read_csv(collectionfile, index_col=0, header=0, delimiter="\t")
-
-
 # Read a file with a single column
 def read_single_column_file(infilename):
 	with open (infilename,'r') as infile:
@@ -34,11 +29,11 @@ def merge_loci(df):
 	for i, (start, end, overlapping_loci) in enumerate(tree.getregions()):
 		if len(overlapping_loci) > 1:
 			marker = ";".join(df.index[overlapping_loci])
-			df_addon.ix[marker,'snps'] = ";".join(df.ix[overlapping_loci,'snps'])
+			df_addon.ix[marker,'snp_id'] = ";".join(df.ix[overlapping_loci,'snp_id'])
 			df_addon.ix[marker,'locus_start'] = start
-			df_addon.ix[marker,'locus_stop'] = end
-			df_addon.ix[marker,'genes_in_locus'] = ";".join(set((";".join([str(x) for x in df.ix[overlapping_loci,'genes_in_locus']])).split(";")))
-			df_addon.ix[marker,'nearest_genes'] = ";".join(df.ix[overlapping_loci,'nearest_genes'])
+			df_addon.ix[marker,'locus_end'] = end
+			df_addon.ix[marker,'genes_in_locus'] = ";".join(set((";".join([ (str(x) if not isinstance(x, float) else '') for x in df.ix[overlapping_loci,'genes_in_locus']])).split(";")))
+			df_addon.ix[marker,'nearest_gene'] = ";".join(df.ix[overlapping_loci,'nearest_gene'])
 			df_addon.ix[marker,'chr'] = df.ix[overlapping_loci[0],'chr']
 			rows2drop.extend(overlapping_loci)
     
@@ -67,32 +62,32 @@ def record_hla_snps(df,df_log,hla_start,hla_stop):
 
 # Function to identify and record non-autosomal SNPs
 def record_sex_chr_snps(df,df_log):
-	non_autosomal_index = df.apply(lambda x : True if x.chr not in [str(y) for y in range(1,23,1)] else False, axis = 1)
+	non_autosomal_index = df.apply(lambda x : True if x.chr in [str(y) for y in range(1,23,1)] else False, axis = 1)
 	if len(df.index[non_autosomal_index]) > 0:
 		df_log.set_value('snps_non-autosomal', 'Markers', ';'.join(df.index[non_autosomal_index]))
 	return non_autosomal_index 
 
 
 # Function to construct gene interval tree
-def get_nearest_gene_intervall_tree(depict_gene_information_file, depictgenes):
+def get_nearest_gene_intervall_tree(depict_gene_information_file, depict_genes):
 	trees = {}
 	for i in range(1, 23, 1):
 		trees[str(i)] = IntervalTree()
 	with open (depict_gene_information_file,'r') as infile:
 		for line in infile.readlines()[1:]:
 			words = line.strip().split('\t')
-			if words[0] in depictgenes and words[4] in [str(x) for x in range(1,23,1)]:
+			if words[0] in depict_genes and words[4] in [str(x) for x in range(1,23,1)]:
 				tss = int(words[5]) if words[7] == '1' else int(words[6])
-				trees[words[4]].insert_interval(Interval(tss, tss, value=words[0])) if words[0] in depictgenes and words[4] in [str(x) for x in range(1,23,1)] else None
+				trees[words[4]].insert_interval(Interval(tss, tss, value=words[0])) if words[0] in depict_genes and words[4] in [str(x) for x in range(1,23,1)] else None
 	return trees
 
 
 # Function to extend boundaries in case of partly overlapping genes
 def get_locus_gene_boundaries(row,df_gene_boundaries):
 	locus_min = row.locus_start 
-	locus_max = row.locus_stop
-	locus_genes = row.nearest_genes.split(';')
-	locus_genes.extend(row.genes_in_locus.split(';')) if row.genes_in_locus != ''  else None
+	locus_max = row.locus_end
+	locus_genes = row.nearest_gene.split(';')
+	locus_genes.extend(row.genes_in_locus.split(';')) if not isinstance(row.genes_in_locus, float)  else None # If it empty then then it is NaN which is a float
 	for gene in set(locus_genes):
 		if gene in df_gene_boundaries.index:
 			gene_start = df_gene_boundaries.ix[gene,'Gene Start (bp)']
@@ -110,8 +105,6 @@ def get_nearest(chrom, pos, trees):
 	my_max_dist = chr_1_bps / 10
 	gene_up = trees[chrom].before(pos,num_intervals=1,max_dist=my_max_dist)
 	gene_down = trees[chrom].after(pos,num_intervals=1,max_dist=my_max_dist)
-	#if pos == 39936815:
-	#	pdb.set_trace()
 
 	# Assuming there will always be a gene_down if there is no gene_up (i.e. we are at the start of the chr)
 	if not gene_up:
@@ -125,24 +118,29 @@ def get_nearest(chrom, pos, trees):
 	return gene_up[0].value if abs(gene_up[0].start - pos) < abs(gene_down[0].start - pos) else gene_down[0].value #, abs(gene_up[0].start - pos) if abs(gene_up[0].start - pos) < abs(gene_down[0].start - pos) else abs(gene_down[0].start - pos)
 
 
+# Helper function to convert column to int
+def convert(x):
+	try:
+		return x.astype(int)
+	except:
+		return x
+
+
 # Read SNPsnap collection and input SNPs from user
-def get_loci(collectionfile,depict_gene_file,depict_gene_information_file,snpfile,outfile,hla_start,hla_stop):
+def write_loci(collectionfile,depict_gene_file,depict_gene_information_file,snpfile,outfile,hla_start,hla_stop):
 
 	# Read users SNPs
 	with open (snpfile,'r') as infile:
 	        usersnps = [line.strip() for line in infile.readlines() ]
 
 	# DEPICT gene universe
-	depictgenes = read_single_column_file(depict_gene_file)
+	depict_genes = read_single_column_file(depict_gene_file)
 	
 	# Read SNPsnap collection
-	collection = read_data(collectionfile)
+	collection = pd.read_csv(collectionfile, index_col=0, header=0, delimiter="\t") #,compression = 'bz2')
 
 	# Extract user SNPs
-	collection_usersnps = collection.ix[usersnps,['rsID','loci_upstream','loci_downstream','ID_genes_in_matched_locus']]
-	collection_usersnps['chr'] = pd.Series([x.split(":")[0] for x in collection_usersnps.index], index=collection_usersnps.index)
-	collection_usersnps['pos'] = pd.Series([x.split(":")[1] for x in collection_usersnps.index], index=collection_usersnps.index)
-	collection_usersnps.rename(columns={'rsID': 'snps', 'loci_upstream': 'locus_start','loci_downstream': 'locus_stop'}, inplace=True)
+	collection_usersnps = collection.loc[usersnps,:]
 	
 	# Identify and remove SNPs missing	
 	df_log = pd.DataFrame()
@@ -152,20 +150,6 @@ def get_loci(collectionfile,depict_gene_file,depict_gene_information_file,snpfil
 	index_snps_non_autosomal = record_sex_chr_snps(collection_usersnps,df_log)
 	collection_usersnps.drop(collection_usersnps.index[ ( index_snps_missing | index_snps_hla ) | index_snps_non_autosomal] , inplace=True)
 	
-	# Limit to genes used in DEPICT. If nearest SNPsnap genes is among DEPICT genes than use random gene from locus genes (CHANGE THIS!)
-	depict_genes_in_snpsnaploci = []
-	for locus in collection_usersnps['ID_genes_in_matched_locus']:
-	    genes = []
-	    if not isinstance(locus, float):
-	        for gene in locus.split(';'):
-	            genes.append(gene) if gene in depictgenes else None
-	    depict_genes_in_snpsnaploci.append(";".join(set(genes))) if genes else depict_genes_in_snpsnaploci.append("")
-	collection_usersnps['genes_in_locus'] = pd.Series(depict_genes_in_snpsnaploci, index=collection_usersnps.index)
-
-	# Add nearest gene
-	trees = get_nearest_gene_intervall_tree(depict_gene_information_file,depictgenes)
-	collection_usersnps['nearest_genes'] = collection_usersnps.apply(lambda x: get_nearest(x.chr, int(x.pos), trees ),axis=1)
-
 	# Redefine locus boundaries based on gene boundaries
 	df_gene_boundaries = pd.read_csv(depict_gene_information_file, header = 0, sep='\t', index_col = 0, usecols = [0,5,6])
 	collection_usersnps['locus_gene_boundaries'] = collection_usersnps.apply(lambda x : get_locus_gene_boundaries(x, df_gene_boundaries), axis = 1)
@@ -174,9 +158,12 @@ def get_loci(collectionfile,depict_gene_file,depict_gene_information_file,snpfil
 	df_final = pd.DataFrame()
 	for chrom in set(collection_usersnps.chr):
 	    df_final = df_final.append(merge_loci(collection_usersnps[collection_usersnps.chr == chrom]))
-	
+
 	# Write loci and log to file
-	df_final.to_csv(outfile, index=False, quoting=0, doublequote = False, sep='\t',columns=["snps","chr","locus_start","locus_stop","nearest_genes","genes_in_locus"])
+	df_final.chr = df_final.chr.apply(convert)
+	df_final.locus_start = df_final.locus_start.apply(convert)
+	df_final.locus_end = df_final.locus_end.apply(convert)
+	df_final.to_csv(outfile, index=False, quoting=0, doublequote = False, sep='\t',columns=["snp_id","chr","locus_start","locus_end","nearest_gene","genes_in_locus"])
 	df_log.to_csv(outfile.replace('.txt','.log'), index=True, quoting=0, doublequote = False, sep='\t')
 
 	return 1
