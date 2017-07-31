9a207a19034002f9f94b320527606b739f5be912
Tune H Pers <tunepers@broadinstitute.org>
2017-07-31 13:57:45 +0200

    Added chr X support

--- src/python/snpsnap_to_depict_collection.py
+++ src/python/snpsnap_to_depict_collection.py
@@ -7,23 +7,22 @@ from bx.intervals.intersection import Interval
 from bx.intervals.intersection import IntervalTree
 from datetime import date
 
-
-depict_data_path = "/home/projects/depict/git/DEPICT/data/"
-depictgenefile = "%s/reconstituted_genesets/reconstituted_genesets_150901.binary.rows.txt"%depict_data_path
-depict_gene_annotation_file = "%s/mapping_and_annotation_files/GPL570ProbeENSGInfo+HGNC_reformatted.txt"%depict_data_path
-
-
 # SNPsnap collections for 1000 Genomes Project phase 3 can be downloaded from http://www.broadinstitute.org/mpg/snpsnap/database_download.html
 # Broad, ld0.5, Pilot: /cvar/jhlab/snpsnap/data/step3/1KG_snpsnap_production_v1_uncompressed_incl_rsID/ld0.5/ld0.5_collection.tab
 # Broad, ld0.5, Phase 3: /cvar/jhlab/snpsnap/data/step3/1KG_snpsnap_production_v2/ld0.5_collection.tab
 # Broad, kb500, Pilot: /cvar/jhlab/snpsnap/data/step3/1KG_snpsnap_production_v1_uncompressed_incl_rsID/kb500/kb500_collection.tab
 # Broad, kb500, Phase 3: /cvar/jhlab/snpsnap/data/step3/1KG_snpsnap_production_v2/EUR/kb500/kb500_collection.tab.gz
+# Yggdrasil, ld0.5 with chr x support, Phase 3: /tools/snpsnap/
 
-geno_flag =  "1000genomespilot"
-#geno_flag = "1000genomesphase3" 
-#collection_id = "ld0.5_collection"
-collection_id = "kb500_collection"
-collection_file = "/home/data/snpsnap/data/{}/{}.tab.gz".format(geno_flag,collection_id)
+depict_data_path = "/Users/tp/Drive/Projects/DEPICT/Code/DEPICT/data"
+depict_gene_annotation_file = "%s/mapping_and_annotation_files/GPL570ProbeENSGInfo+HGNC_reformatted.txt"%depict_data_path
+collection_file = "/Volumes/SANDISK256/depict-x/ld0.5_collection.EUR.incl_chrX.2015-07.tab.gz"
+geno_flag = "1000genomesphase3" 
+collection_id = "ld0.5_collection"
+chr_start=1
+chr_end=23
+#geno_flag =  "1000genomespilot"
+#collection_id = "kb500_collection"
 
 
 # Function construct tree with gene intervals
@@ -39,9 +38,10 @@ def get_nearest_gene_intervall_tree(depict_gene_annotation_file, depictgenes):
     with open (depict_gene_annotation_file,'r') as infile:
         for line in infile.readlines()[1:]:
             words = line.strip().split('\t')
-            if words[ens_col] in depictgenes and words[chr_col] in [str(x) for x in range(1,23,1)]:
+            #if words[ens_col] in depictgenes and words[chr_col] in [str(x) for x in range(1,23,1)]:
+            if words[ens_col] in depictgenes:
                 tss = int(words[sta_col]) if words[str_col] == '1' else int(words[end_col])
-                trees[words[chr_col]].insert_interval(Interval(tss, tss, value=words[ens_col])) if words[ens_col] in depictgenes and words[chr_col] in [str(x) for x in range(1,23,1)] else None
+                trees[words[chr_col]].insert_interval(Interval(tss, tss, value=words[ens_col])) if words[ens_col] in depictgenes and words[chr_col] in [str(x) for x in range(chr_start,chr_end+1,1)] else None
     return trees
 
 
@@ -52,14 +52,14 @@ def get_nearest(chrom, pos):
 
     # Assuming there will always be a gene_down if there is no gene_up (i.e. we are at the start of the chr)
     if not gene_up:
-        return gene_down[0].value #, abs(gene_down[0].start - pos)
+        return gene_down[0].value
     
     # Assuming there will always be a gene_up if there is no gene_down (i.e. we are at the end of the chr)
     elif not gene_down:
-        return gene_up[0].value #, abs(gene_up[0].start - pos)
+        return gene_up[0].value
 
     # Test whether upstream or downstream gene
-    return gene_up[0].value if abs(gene_up[0].start - pos) < abs(gene_down[0].start - pos) else gene_down[0].value #, abs(gene_up[0].start - pos) if abs(gene_up[0].start - pos) < abs(gene_down[0].start - pos) else abs(gene_down[0].start - pos)
+    return gene_up[0].value if abs(gene_up[0].start - pos) < abs(gene_down[0].start - pos) else gene_down[0].value
 
 
 # Function to filter out genes not in DEPICT
@@ -81,13 +81,13 @@ collection['pos'] = pd.Series([x.split(":")[1] for x in collection.index], index
 
 
 # Exclude non-autosomal SNPs
-non_autosomal_index = collection.apply(lambda x : True if x.chr in [str(y) for y in range(23,25,1)] else False, axis = 1)
-collection.drop(collection.index[non_autosomal_index], inplace=True)
+#non_autosomal_index = collection.apply(lambda x : True if x.chr in [str(y) for y in range(23,25,1)] else False, axis = 1)
+#collection.drop(collection.index[non_autosomal_index], inplace=True)
 
 
 # Read genes covered by DEPICT
-with open (depictgenefile,'r') as infile:
-    depictgenes = [line.strip() for line in infile.readlines()]
+with open (depict_gene_annotation_file,'r') as infile:
+    depictgenes = [line.strip().split("\t")[0] for line in infile.readlines()[1:]]
 
 
 # Construct gene interval tree and identify nearest genes for all SNPs in the collection
